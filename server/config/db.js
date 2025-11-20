4f10f931c395e721207050bac1bbdcd0f492a65e
siphc <ayfpan@ucla.edu>
2025-11-20 15:25:04 -0800

    Refactor test suite w/ jest

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,19 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    let uri;
+    
+    if (process.env.NODE_ENV === 'test') {
+      uri = process.env.MONGODB_TEST_URI;
+      if (!uri) {
+        throw new Error('MONGODB_TEST_URI must be set when NODE_ENV===test');
+      }
+    }
+    else {
+      uri = process.env.MONGODB_URI;
+    }
+    
+    await mongoose.connect(uri);
     console.log('MongoDB connected successfully');
     console.log(`Database: ${mongoose.connection.name}`);
   } catch (error) {
